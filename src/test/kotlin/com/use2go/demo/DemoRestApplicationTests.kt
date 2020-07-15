66816f0ecea0f5982f038ec07340173e9c052188
Dmitry Zapashchikov <dmitriiz@hotmail.com>
2020-07-16 00:01:19 +0300

    test added

--- src/test/kotlin/com/use2go/demo/DemoRestApplicationTests.kt
+++ src/test/kotlin/com/use2go/demo/DemoRestApplicationTests.kt
@@ -1,13 +1,41 @@
 package com.use2go.demo
 
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.ApplicationContext
+import org.springframework.restdocs.JUnitRestDocumentation
+import org.springframework.test.web.reactive.server.WebTestClient
+
 
 @SpringBootTest
 class DemoRestApplicationTests {
 
+	@Autowired
+	private lateinit var context: ApplicationContext
+
+//	@Rule
+//	private lateinit val restDocumentation: JUnitRestDocumentation
+
+
+	private lateinit var webClient: WebTestClient
+
+	@BeforeEach
+	internal fun setUp() {
+		webClient = WebTestClient
+				.bindToApplicationContext(context)
+				.configureClient()
+				.baseUrl("http://localhost:8080")
+				.build()
+	}
+
 	@Test
-	fun contextLoads() {
+	fun testGetNoUsers() {
+		webClient.get().exchange()
+				.expectStatus().isOk
+				.expectBody().json("[]")
 	}
 
 }
